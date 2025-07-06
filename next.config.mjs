bbdae8d3fd3e0f3ec5f6b714ec7beef935a3d755
Manish Chand <manishtochand@gmail.com>
2025-07-06 16:07:19 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverExternalPackages: ['@prisma/client'],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), '@prisma/client'];
+    return config;
+  },
+};
 
 export default nextConfig;
